7e537dd643fd86f60089fe63660992937cf5e0bd
Samira Zein <samirazein20@gmail.com>
2020-08-09 22:00:30 -0500

    Project SetUp and Domain Design

---
+++ src/main/java/com/example/demo/controller/test.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+    @GetMapping("/")
+    public String getId(){
+      return "Hello There";
+    }
+}
