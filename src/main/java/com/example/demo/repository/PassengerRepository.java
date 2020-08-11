7672f899107499cb432f1210551d6f3e45132afb
kisakyegordon <gordon56692@gmail.com>
2020-08-11 11:33:14 -0500

    removes errors on running

---
+++ src/main/java/com/example/demo/repository/PassengerRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.Passenger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PassengerRepository extends JpaRepository<Passenger, Long> {
+}
