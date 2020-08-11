c2843ad0c7d6ae146a34548e709787df14606740
Samira Zein <samirazein20@gmail.com>
2020-08-11 11:43:19 -0500

    Few Bug fixes and Swagger Configuration for API documentation

---
+++ src/main/java/com/example/demo/configuration/swagger.java
@@ -0,0 +1,76 @@
+package com.example.demo.configuration;
+
+import com.google.common.collect.Lists;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Date;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class swagger extends WebMvcConfigurationSupport {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    @Bean
+    public Docket api() {
+
+        Docket docket = new Docket(DocumentationType.SWAGGER_2).apiInfo(apiEndPointsInfo()).pathMapping("/")
+                .forCodeGeneration(true).genericModelSubstitutes(ResponseEntity.class)
+                .ignoredParameterTypes(SpringDataWebProperties.Pageable.class).ignoredParameterTypes(java.sql.Date.class)
+                .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
+                .directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
+                .directModelSubstitute(java.time.LocalDateTime.class, Date.class)
+                .securityContexts(Lists.newArrayList(securityContext())).securitySchemes(Lists.newArrayList(apiKey()))
+                .useDefaultResponseMessages(false);
+
+        docket = docket.select().paths(PathSelectors.regex("/.*")).build();
+        // watch.stop();
+        return docket;
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Spring Boot REST API").description("Rental Management REST API Documentation")
+                .contact(new Contact("Code Busters", "https://github.com/samirazein20/", "samirazein20@gmail.com"))
+                .license("Apache 2.0").licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html").version("1.0.0")
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.regex("/api/.*"))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
--- src/main/java/com/example/demo/repository/TicketRepository.java
+++ src/main/java/com/example/demo/repository/TicketRepository.java
@@ -7,7 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     public Ticket getTicketsByFlightNumber(Integer flightNumber);
-    public Ticket getTicketsByFlightName(String flightName);
-    public Ticket getTicketByPassengerName(String name);
-    public Ticket getTicketByPassengerEmail(String email);
 }
