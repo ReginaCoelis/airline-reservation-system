0f58213dce35d8aaa5db2baff4c907d6258e63a1
Rachel Aitaru <aitarurachel@gmail.com>
2020-08-10 22:13:43 -0500

    Flight implementation

---
+++ src/main/java/com/example/demo/service/FlightService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Flight;
+import com.example.demo.dto.request.FlightRequest;
+import com.example.demo.dto.response.FlightResponse;
+import com.example.demo.service.impl.FlightServiceImpl;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface FlightService {
+    public FlightResponse addFlight(FlightRequest requestFlight );
+    public FlightResponse getFlightByNumber(Integer FlightNumber);
+    public List<FlightResponse>getAllFlights();
+
+}
---
+++ src/main/java/com/example/demo/service/impl/FlightServiceImpl.java
@@ -0,0 +1,79 @@
+package com.example.demo.service.impl;
+
+
+import com.example.demo.domain.Airline;
+import com.example.demo.domain.Airport;
+import com.example.demo.domain.Flight;
+import com.example.demo.dto.request.FlightRequest;
+import com.example.demo.dto.response.FlightResponse;
+import com.example.demo.repository.AirlineRepository;
+import com.example.demo.repository.AirportRepository;
+import com.example.demo.repository.FlightRepository;
+import com.example.demo.service.FlightService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+@Transactional
+public class FlightServiceImpl implements FlightService {
+
+    @Autowired
+    private FlightRepository flightRepository;
+
+    @Autowired
+    private AirlineRepository airlineRepository;
+
+    @Autowired
+    private AirportRepository airportRepository;
+
+
+
+    @Override
+    public FlightResponse addFlight(FlightRequest requestFlight) {
+        Airline airline= airlineRepository.findByCode(requestFlight.getAirline().getCode());
+        Airport departureAirport= airportRepository.findByCode(requestFlight.getDepartureAirport().getCode());
+        Airport originAirport= airportRepository.findByCode(requestFlight.getArrivalAirport().getCode());
+
+        Flight flight=new Flight();
+        flight.setDepartureAirport(departureAirport);
+        flight.setArrivalAirport(originAirport);
+        flight.setFlightNumber(requestFlight.getFlightNumber());
+        flight.setCapacity(requestFlight.getCapacity());
+        flight.setDepartureTime(requestFlight.getDepartureTime());
+        flight.setDepartureDate(requestFlight.getDepartureDate());
+        flight.setArrivalTime(requestFlight.getArrivalTime());
+        flight.setArrivalDate(requestFlight.getArrivalDate());
+        flight.setAirline(airline);
+
+        return covertFlightToFlightResponse(flightRepository.save(flight));
+
+
+    }
+
+    @Override
+    public FlightResponse getFlightByNumber(Integer flightNumber) {
+        return covertFlightToFlightResponse(flightRepository.getByFlightNumber(flightNumber));
+
+    }
+
+    @Override
+    public List<FlightResponse> getAllFlights() {
+        return flightRepository.findAll().stream().parallel().map(this::covertFlightToFlightResponse).collect(Collectors.toList());
+
+
+    }
+
+    private FlightResponse covertFlightToFlightResponse(Flight flight){
+
+        return new FlightResponse();
+
+    }
+
+
+
+}
