9bd68aded03cd12ebdbddd3ff253445c58285b22
kisakyegordon <gordon56692@gmail.com>
2020-08-10 23:09:49 -0500

    flight-service-functionality

--- src/main/java/com/example/demo/domain/Passenger.java
+++ src/main/java/com/example/demo/domain/Passenger.java
@@ -15,4 +15,19 @@ public class Passenger {
     private String role;
     private String email;
 
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public String getRole() { return role; }
+
+    public void setRole(String role) { this.role = role; }
 }
--- src/main/java/com/example/demo/service/FlightService.java
+++ src/main/java/com/example/demo/service/FlightService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.domain.Flight;
+import com.example.demo.domain.Passenger;
 import com.example.demo.dto.request.FlightRequest;
 import com.example.demo.dto.response.FlightResponse;
 import com.example.demo.service.impl.FlightServiceImpl;
@@ -13,5 +14,7 @@ public interface FlightService {
     public FlightResponse addFlight(FlightRequest requestFlight );
     public FlightResponse getFlightByNumber(Integer FlightNumber);
     public List<FlightResponse>getAllFlights();
+    public Flight getFlightByPassengerId(Integer passengerId);
+    public void bookFlight(int flightId, Passenger passenger);
 
 }
