9ad1c4a12786fdfafc51e517ceb3b00eafdba79b
kisakyegordon <gordon56692@gmail.com>
2020-08-12 16:31:56 -0500

    fix

--- src/main/java/com/airline/reservation/domain/Passenger.java
+++ src/main/java/com/airline/reservation/domain/Passenger.java
@@ -27,6 +27,18 @@ public class Passenger implements UserDetails {
         return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     @Override
     public String getPassword() {
         return password;
