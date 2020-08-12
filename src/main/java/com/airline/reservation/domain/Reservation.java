f1ab52478dfa4683415d20267b6db11956033144
kisakyegordon <gordon56692@gmail.com>
2020-08-12 12:00:29 -0500

    fix bookings

--- src/main/java/com/airline/reservation/domain/Reservation.java
+++ src/main/java/com/airline/reservation/domain/Reservation.java
@@ -35,4 +35,84 @@ public class Reservation {
     private Date lastUpdateDate;
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Integer> getFlightNumbers() {
+        return flightNumbers;
+    }
+
+    public void setFlightNumbers(Set<Integer> flightNumbers) {
+        this.flightNumbers = flightNumbers;
+    }
+
+    public Long getPassengerId() {
+        return passengerId;
+    }
+
+    public void setPassengerId(Long passengerId) {
+        this.passengerId = passengerId;
+    }
+
+    public Long getMadeByUserId() {
+        return madeByUserId;
+    }
+
+    public void setMadeByUserId(Long madeByUserId) {
+        this.madeByUserId = madeByUserId;
+    }
+
+    public Long getMadeByAgentId() {
+        return madeByAgentId;
+    }
+
+    public void setMadeByAgentId(Long madeByAgentId) {
+        this.madeByAgentId = madeByAgentId;
+    }
+
+    public boolean isConfirmed() {
+        return isConfirmed;
+    }
+
+    public void setConfirmed(boolean isConfirmed) {
+        this.isConfirmed = isConfirmed;
+    }
+
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+
+    public void setCancelled(boolean isCancelled) {
+        this.isCancelled = isCancelled;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(Date lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public String getReservationCode() {
+        return reservationCode;
+    }
+
+    public void setReservationCode(String reservationCode) {
+        this.reservationCode = reservationCode;
+    }
+
 }
--- src/main/java/com/airline/reservation/exception/EmailExistsException.java
+++ src/main/java/com/airline/reservation/exception/EmailExistsException.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 public class EmailExistsException extends RuntimeException {
 
     public EmailExistsException(String message){
-        log.info("Email: {}",message);
+//        log.info("Email: {}",message);
+        System.out.println(message);
     }
 }
